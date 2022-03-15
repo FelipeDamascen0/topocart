96b23d2d1cd4c2c2eac7c4efca5e8fa06af0bad3
Felipe Damasceno de Oliveira <felipeodamasceno@hotmail.com>
2022-03-15 16:06:07 -0300

    Projeto topocars

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import formJson from './form.json';
 import './App.css';
 
 function App() {
+  const [form, setForm] = useState(formJson.campos)
+  const [selecao, setSelecao] = useState(1)
+  const [email, setEmail] = useState()
+  const [senha, setSenha] = useState()
+  const [numero, setNumero] = useState()
+
+  console.log(email)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        form.map((i) => {
+          return (
+            <form>
+
+              {
+                i.type === 'select' ?
+
+                  <select value={selecao} onChange={(e) => {
+                    let value = e.target.value
+                    setSelecao(value)
+                  }}>
+                    {
+                      i.options.map((o) => {
+                        return (
+                          <option key={o.value} value={o.value}>{o.text}</option>
+                        )
+                      })
+                    }
+                  </select>
+
+                  :
+                  <input type={i.type} placeholder={i.label} name={i.name} key={i.id} onChange={(e) => {
+                    if(i.type === 'email'){
+                      let valorEmail = e.target.value
+                      setEmail(valorEmail)
+                    }
+
+                    if(i.type === 'password'){
+                      let valorSenha = e.target.value 
+                      setSenha(valorSenha)
+                    }
+
+                    if(i.type === 'number'){
+                      let valorNumero = e.target.value 
+                      setNumero(valorNumero)
+                    }
+                  }} />
+              }
+            </form>
+          )
+        })
+      }
+      <button
+        onClick={(e) => {
+          e.preventDefault()
+          console.log(`
+          Email: ${email} 
+          Senha: ${senha} 
+          Numero: ${numero} 
+          Selecao: ${selecao}
+          `)
+        }}
+      > 
+        Enviar
+      </button>
     </div>
   );
 }
+
 
 export default App;
